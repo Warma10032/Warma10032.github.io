7f71e9ba3446b2ea6d029a24c5fe527a4a01ae39
“Warma10032” <1601302408@qq.com>
2024-03-07 22:49:12 +0800

    Site updated: 2024-03-07 22:49:01

---
+++ js/formal.js
@@ -0,0 +1,39 @@
+// 复制提醒
+document.addEventListener("copy", function () {
+  debounce(function () {
+    new Vue({
+      data: function () {
+        this.$notify({
+          title: "哎嘿！复制成功🍬",
+          message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+          position: 'top-left',
+          offset: 50,
+          showClose: true,
+          type: "success",
+          duration: 5000
+        });
+      }
+    })
+  }, 300);
+})
+
+// f12提醒但不禁用
+document.onkeydown = function (e) {
+  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+    debounce(function () {
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+          });
+        }
+      })
+    }, 300);
+  }
+};
